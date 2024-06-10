8f782a4d1e03785c13149b4960089ec50121483b
qistinaazli <nurqistina.noorazli@gmail.com>
2024-06-10 10:58:13 +0800

    Add files via upload

---
+++ Machine_Status.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PumpDashboard
+{
+	public class Machine_Status
+	{
+		[DataType("mach_id")]
+		public int Machine_Id { get; set; }
+
+		[DataType("is_running")]
+		public bool Is_Running { get; set; }
+	}
+}
---
+++ Volume_List.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PumpDashboard
+{
+    public class Volume_List
+    {
+        [DataType("volume")]
+        public double Volume { get; set; }
+
+        [DataType("total_volume")]
+        public double Total_Volume { get; set; }
+
+        [DataType("date_time")]
+        public DateTime Date_Time { get; set; }
+    }
+}
